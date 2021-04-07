c6615a323685d872759e1afdbb29437f5d53e7cf
artall64 <artall64@gmail.com>
2021-04-08 01:43:18 +0300

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {asyncScheduler, from, merge, of} from 'rxjs';
+import {concatMap, delay, tap, throttleTime} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+
+  constructor() {
+    const stream1$ = from([1, 2, 3, 4, 5]).pipe(
+      concatMap((elem) => {
+        return of(elem).pipe(
+          delay(500)
+        );
+      })
+    );
+
+    const stream2$ = from(['EVENT']).pipe(
+      delay(1100)
+    );
+
+
+    // tslint:disable-next-line:no-console
+    console.time('profiling');
+
+    merge(stream1$, stream2$).pipe(
+      throttleTime(110, asyncScheduler, {leading: false, trailing: true})
+    ).subscribe({
+      next: (x) => {
+        console.timeLog('profiling', x);
+      },
+      error: () => {
+      }
+    });
+  }
 }
